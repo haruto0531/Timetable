d99896231f6134e90f15a8e0308ccd3ff12946f8
owner <saikunki@yahoo.co.jp>
2019-08-16 21:30:24 +0900

    Initial Commit

---
+++ Timetable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timetable
+//
+//  Created by owner on 2019/08/16.
+//  Copyright © 2019 owner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
